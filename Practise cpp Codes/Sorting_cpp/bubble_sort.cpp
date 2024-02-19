9bd7f3b0d773d1537ed0a454821fb3a6a138fd09
TUSHAR SINGH <tusharsinghrawat.delhi@gmail.com>
2024-02-19 12:18:05 +0530

    Add files via upload

---
+++ Practise cpp Codes/Sorting_cpp/bubble_sort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[] , int n){
+    for(int i=n-1; i>=0; i--){
+        bool did_swap = false;
+        for(int j=0; j<=i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                did_swap = true;
+            } 
+        }if(did_swap == false) break;
+    }
+
+    cout<<"arr after selection sort: "<<endl;
+    for(int k=0; k<n; k++){
+        cout<<arr[k]<<" ";
+    }
+}
+
+int main(){
+
+    int size;
+    cout<<"enter the size of arr: ";
+    cin>>size;
+
+    int arr[size];
+    cout<<"enter the elements of arr: ";
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+    //fun_call
+    bubble_sort(arr,size);
+
+    return 0;
+}
