fa91ac1f75aa475c2ad09f8c76bde43daa3f0069
SINGHxTUSHAR <tusharsinghrawat.delhi@gmail.com>
2024-09-27 14:00:04 +0530

    update

--- Leetcode-Solutions/Power_of_Two.cpp
+++ Leetcode-Solutions/Power_of_Two.cpp
@@ -17,15 +17,29 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
+        /*Basic check & divison*/
         //edge case
         if(n==0) return false;
         
         //if n is divisible by 2,  find its quotient untill its last one
         while(n%2==0) n/=2;
-    
-        if(n==1) return true;
-        else return false;
+        return (n==1) ? true : false;
+
+        /*Bit magic*/
+        // if(n<=0) return false;
+        // return (n&(n-1)) == 0;
+
+        /*Built in function STL*/
+        // if(n <= 0) return false;
+        // // __builtin_popcount() returns the no. of set bits
+        // // sine the power of 2 has only 1 set bit
+        // int cnt = __builtin_popcount(n);
+        // return (cnt == 1) ? true : false;
+
+        /*recursion*/
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        return n%2 == 0 && isPowerOfTwo(n/2);
     }
 };
 
